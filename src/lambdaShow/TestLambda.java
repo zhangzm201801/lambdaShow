247551447141150bc79c46a777bf438141069ae8
zhangzm201801 <zhangzm1117@126.com>
2019-06-30 15:35:06 +0900

    lambda 推导

--- src/lambdaShow/TestLambda.java
+++ src/lambdaShow/TestLambda.java
@@ -1,10 +1,25 @@
 package lambdaShow;
 
+/*
+ * 推到lambda表达式
+ */
 public class TestLambda {
+	
+	static class Like2 implements ILike{
+
+		@Override
+		public void lambda() {
+			System.out.println("I like lambda2");
+		}
+	}
 	
 	public static void main(String[] args) {
 		ILike like = new Like();
 		like.lambda();
+		
+		like = new Like2();
+		like.lambda();
+		
 	}
 }
 
@@ -19,5 +34,5 @@ class Like implements ILike{
 	public void lambda() {
 		System.out.println("I like lambda");
 	}
-	
+
 }
